43a691b9bc115b5898d0dc8f86fd703edf004684
Dan Sabin <dsabin@beatsmusic.com>
2014-05-04 12:27:56 -0700

    inital commit on sdk

---
+++ lib/src/main/java/com/freethinking/beats/sdk/data/BaseJson.java
@@ -0,0 +1,10 @@
+package com.freethinking.beats.sdk.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract class BaseJson implements Serializable {
+    public abstract void fillIn(BaseJson parseJson) throws Exception;
+}
